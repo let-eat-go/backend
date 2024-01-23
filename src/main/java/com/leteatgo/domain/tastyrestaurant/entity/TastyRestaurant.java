fac177d50b84f18b76bf5813c04999adcdbbc008
do5do <do151015@gmail.com>
2024-01-23 13:49:24 +0900

    chore: setting 수정 - 맛집 도메인 패키지 명 수정

---
+++ src/main/java/com/leteatgo/domain/tastyrestaurant/entity/TastyRestaurant.java
@@ -0,0 +1,56 @@
+package com.leteatgo.domain.tastyRestaurant.entity;
+
+import com.leteatgo.global.type.RestaurantCategory;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(
+        name = "tasty_restaurant",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "uk_tasty_restaurant_kakao_id", columnNames = {"kakao_id"}
+                )
+        }
+)
+public class TastyRestaurant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "kakao_id", nullable = false)
+    private Long kakaoId;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "category", nullable = false)
+    private RestaurantCategory category;
+
+    @Column(name = "phone_number", nullable = false)
+    private String phoneNumber;
+
+    @Column(name = "road_address", nullable = false)
+    private String roadAddress;
+
+    @Column(name = "land_address", nullable = false)
+    private String landAddress;
+
+    @Column(name = "latitude", nullable = false)
+    private Double latitude;
+
+    @Column(name = "longitude", nullable = false)
+    private Double longitude;
+
+    @Column(name = "restaurant_url", nullable = false)
+    private String restaurantUrl;
+
+    @Column(name = "number_of_uses", nullable = false)
+    private Integer numberOfUses;
+    
+}
