fed263ee55134e7d3e78a11cdd0609d5b1c1c5a9
do5do <do151015@gmail.com>
2024-01-23 13:03:29 +0900

    chore: setting 수정 - CustomException 빠진 부분 추가

--- src/main/java/com/leteatgo/global/exception/CustomException.java
+++ src/main/java/com/leteatgo/global/exception/CustomException.java
@@ -3,16 +3,19 @@ package com.leteatgo.global.exception;
 import lombok.Getter;
 
 @Getter
-public class CustomException extends RuntimeException {
+public abstract class CustomException extends RuntimeException {
 
     private ErrorCode errorCode;
     private String message;
 
     public CustomException(ErrorCode errorCode) {
+        super(errorCode.getErrorMessage());
         this.errorCode = errorCode;
+        this.message = errorCode.getErrorMessage();
     }
 
     public CustomException(ErrorCode errorCode, String message) {
+        super(message);
         this.errorCode = errorCode;
         this.message = message;
     }
