f52dd0e807b9fe0dcfa81fddf36c0f7ded457ab3
Kingseungil <flykim511@naver.com>
2024-01-30 11:23:31 +0900

    chore: Meeting Entity
    - Time -> LocalTime으로 변경

--- src/main/java/com/leteatgo/domain/meeting/entity/Meeting.java
+++ src/main/java/com/leteatgo/domain/meeting/entity/Meeting.java
@@ -5,11 +5,12 @@ import com.leteatgo.domain.tastyrestaurant.entity.TastyRestaurant;
 import com.leteatgo.global.entity.BaseEntity;
 import com.leteatgo.global.type.*;
 import jakarta.persistence.*;
+import java.time.LocalTime;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.sql.Time;
 import java.time.LocalDate;
 
 @Entity
@@ -27,13 +28,13 @@ public class Meeting extends BaseEntity {
     private Member host;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "tasty_restaurant_id", nullable = false)
+    @JoinColumn(name = "tasty_restaurant_id", nullable = true)
     private TastyRestaurant tastyRestaurant;
 
     @Column(name = "name", nullable = false)
     private String name;
 
-    @Column(name = "restaurant_category", nullable = false)
+    @Column(name = "restaurant_category", nullable = false, length = 50)
     @Enumerated(EnumType.STRING)
     private RestaurantCategory restaurantCategory;
 
@@ -50,12 +51,30 @@ public class Meeting extends BaseEntity {
     private LocalDate start_date;
 
     @Column(name = "start_time", nullable = false)
-    private Time start_time;
+    private LocalTime start_time;
 
     @Column(name = "description", nullable = false)
     private String description;
 
     @Embedded
     private MeetingOptions meetingOptions;
+
+    @Builder
+    public Meeting(Member host, TastyRestaurant tastyRestaurant, String name,
+            RestaurantCategory restaurantCategory, String region, Integer minParticipants,
+            Integer maxParticipants, LocalDate start_date, LocalTime start_time, String description,
+            MeetingOptions meetingOptions) {
+        this.host = host;
+        this.tastyRestaurant = tastyRestaurant;
+        this.name = name;
+        this.restaurantCategory = restaurantCategory;
+        this.region = region;
+        this.minParticipants = minParticipants;
+        this.maxParticipants = maxParticipants;
+        this.start_date = start_date;
+        this.start_time = start_time;
+        this.description = description;
+        this.meetingOptions = meetingOptions;
+    }
 
 }
