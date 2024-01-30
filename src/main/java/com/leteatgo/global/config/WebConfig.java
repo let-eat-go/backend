6d2b85e1c2f8c194e03857250fc39b3752a10b70
Kingseungil <109774037+kingseungil@users.noreply.github.com>
2024-01-30 14:40:21 +0900

    [#11] dev브랜치 CI/CD 구축 (#18)
    * feat: docker 설정
    
    - 임시 테스트용
    
    * feat: 도커 빌드 스크립트 작성
    
    - 환경별로 빌드될 수 있도록 Dockerfile 정의
    
    * feat: CI/CD Test
    
    * feat: CI/CD Test
    
    * feat: dev브랜치 CI/CD
    
    - webconfig 추가
    - github workflows 추가
    
    * chore: 단순 수정
    
    * fix: 로그인 시 토큰 생성 수정
    
    - RedisToken 객체만들때 refreshtoken이랑 accesstoken 위치가 바껴있었네여..어쩐지 토큰 재발급이 안되더라구요

---
+++ src/main/java/com/leteatgo/global/config/WebConfig.java
@@ -0,0 +1,28 @@
+package com.leteatgo.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebMvc
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true); // 쿠키 허용
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/docs/**")
+                .addResourceLocations("classpath:/static/docs/");
+    }
+
+}
