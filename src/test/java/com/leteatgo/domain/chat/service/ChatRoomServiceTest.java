df2b139e2b9ea23cc7bd017de1f47d9d177f253a
do5do <do151015@gmail.com>
2024-02-01 17:21:39 +0900

    test: 테스트 코드 작성

---
+++ src/test/java/com/leteatgo/domain/chat/service/ChatRoomServiceTest.java
@@ -0,0 +1,271 @@
+package com.leteatgo.domain.chat.service;
+
+
+import static com.leteatgo.domain.chat.type.RoomStatus.OPEN;
+import static com.leteatgo.global.exception.ErrorCode.NOT_FOUND_CHATROOM;
+import static com.leteatgo.global.exception.ErrorCode.NOT_FOUND_MEETING;
+import static com.leteatgo.global.exception.ErrorCode.NOT_FOUND_MEMBER;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+
+import com.leteatgo.domain.chat.dto.response.ChatMessageResponse;
+import com.leteatgo.domain.chat.dto.response.MyChatRoomResponse;
+import com.leteatgo.domain.chat.entity.ChatMessage;
+import com.leteatgo.domain.chat.entity.ChatRoom;
+import com.leteatgo.domain.chat.event.dto.CloseChatRoomEvent;
+import com.leteatgo.domain.chat.event.dto.CreateChatRoomEvent;
+import com.leteatgo.domain.chat.exception.ChatException;
+import com.leteatgo.domain.chat.repository.ChatMessageRepository;
+import com.leteatgo.domain.chat.repository.ChatRoomRepository;
+import com.leteatgo.domain.meeting.entity.Meeting;
+import com.leteatgo.domain.meeting.repository.MeetingParticipantRepository;
+import com.leteatgo.domain.meeting.repository.MeetingRepository;
+import com.leteatgo.domain.member.entity.Member;
+import com.leteatgo.domain.member.exception.MemberException;
+import com.leteatgo.global.dto.CustomPageRequest;
+import com.leteatgo.global.security.CustomUserDetailService;
+import com.leteatgo.global.type.RestaurantCategory;
+import java.util.List;
+import java.util.Optional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.SliceImpl;
+import org.springframework.test.util.ReflectionTestUtils;
+
+@ExtendWith(MockitoExtension.class)
+class ChatRoomServiceTest {
+
+    @Mock
+    MeetingRepository meetingRepository;
+
+    @Mock
+    ChatRoomRepository chatRoomRepository;
+
+    @Mock
+    ChatMessageRepository chatMessageRepository;
+
+    @Mock
+    CustomUserDetailService customUserDetailService;
+
+    @Mock
+    MeetingParticipantRepository meetingParticipantRepository;
+
+    @InjectMocks
+    ChatRoomService chatRoomService;
+
+    @Nested
+    @DisplayName("채팅방 개설 메서드는")
+    class CreateChatRoomMethod {
+
+        Long meetingId = 1L;
+        Meeting meeting = Meeting.builder().build();
+        ChatRoom chatRoom = new ChatRoom(OPEN, meeting);
+        CreateChatRoomEvent event = new CreateChatRoomEvent(meetingId);
+
+        @Test
+        @DisplayName("생성된 모임이면 채팅방 개설에 성공한다.")
+        void createChatRoom() {
+            // given
+            given(meetingRepository.findById(meetingId))
+                    .willReturn(Optional.of(meeting));
+
+            given(chatRoomRepository.save(any()))
+                    .willReturn(chatRoom);
+
+            // when
+            // then
+            assertThatCode(() -> chatRoomService.createChatRoom(event))
+                    .doesNotThrowAnyException();
+        }
+
+        @Test
+        @DisplayName("존재하지 않는 모임이면 예외를 발생시킨다.")
+        void createChatRoom_meeting_not_found() {
+            // given
+            given(meetingRepository.findById(meetingId))
+                    .willThrow(new ChatException(NOT_FOUND_MEETING));
+
+            // when
+            // then
+            assertThatThrownBy(() -> chatRoomService.createChatRoom(event))
+                    .isInstanceOf(ChatException.class)
+                    .hasMessageContaining(NOT_FOUND_MEETING.getErrorMessage());
+        }
+    }
+
+    @Nested
+    @DisplayName("채팅방 종료 메서드는")
+    class CloseChatRoomMethod {
+        Long meetingId = 1L;
+        Meeting meeting = Meeting.builder().build();
+        ChatRoom chatRoom = new ChatRoom(OPEN, meeting);
+        CloseChatRoomEvent event = new CloseChatRoomEvent(meetingId);
+
+        @Test
+        @DisplayName("모임이 취소, 종료되면 채팅방이 종료됩니다.")
+        void closeChatRoom() {
+            // given
+            given(meetingRepository.findById(meetingId))
+                    .willReturn(Optional.of(meeting));
+
+            given(chatRoomRepository.findByMeeting(meeting))
+                    .willReturn(Optional.of(chatRoom));
+
+            // when
+            // then
+            assertThatCode(() -> chatRoomService.closeChatRoom(event))
+                    .doesNotThrowAnyException();
+        }
+
+        @Test
+        @DisplayName("존재하지 않는 모임이면 예외를 발생시킨다.")
+        void createChatRoom_meeting_not_found() {
+            // given
+            given(meetingRepository.findById(meetingId))
+                    .willThrow(new ChatException(NOT_FOUND_MEETING));
+
+            // when
+            // then
+            assertThatThrownBy(() -> chatRoomService.closeChatRoom(event))
+                    .isInstanceOf(ChatException.class)
+                    .hasMessageContaining(NOT_FOUND_MEETING.getErrorMessage());
+        }
+
+        @Test
+        @DisplayName("존재하지 않는 채팅방이면 예외를 발생시킨다.")
+        void createChatRoom_chatRoom_not_found() {
+            // given
+            given(meetingRepository.findById(meetingId))
+                    .willReturn(Optional.of(meeting));
+
+            given(chatRoomRepository.findByMeeting(meeting))
+                    .willThrow(new ChatException(NOT_FOUND_CHATROOM));
+
+            // when
+            // then
+            assertThatThrownBy(() -> chatRoomService.closeChatRoom(event))
+                    .isInstanceOf(ChatException.class)
+                    .hasMessageContaining(NOT_FOUND_CHATROOM.getErrorMessage());
+        }
+    }
+
+    @Nested
+    @DisplayName("채팅방 대화 목록 조회 메서드는")
+    class RoomMessagesMethod {
+        Long roomId = 1L;
+        CustomPageRequest customPageRequest = new CustomPageRequest(1);
+        ChatRoom chatRoom = new ChatRoom(OPEN, Meeting.builder().build());
+        Pageable pageable = PageRequest.of(0, CustomPageRequest.PAGE_SIZE);
+        Member member = Member.builder()
+                .nickname("nick")
+                .profileImage("profile")
+                .build();
+        ChatMessage chatMessage = new ChatMessage("message");
+        List<ChatMessage> contents = List.of(chatMessage);
+
+        @BeforeEach
+        void setup() {
+            ReflectionTestUtils.setField(member, "id", 1L);
+            chatMessage.setSender(member);
+        }
+
+        @Test
+        @DisplayName("존재하는 채팅방이면 대화 목록을 조회할 수 있다.")
+        void roomMessages() {
+            // given
+            given(chatRoomRepository.findById(roomId))
+                    .willReturn(Optional.of(chatRoom));
+
+            given(chatMessageRepository.findByChatRoomFetch(chatRoom, pageable))
+                    .willReturn(new SliceImpl<>(contents, pageable, true));
+
+            // when
+            Slice<ChatMessageResponse> chatMessageResponses =
+                    chatRoomService.roomMessages(roomId, customPageRequest);
+
+            // then
+            assertEquals(1, chatMessageResponses.getContent().size());
+            assertTrue(chatMessageResponses.getContent().get(0).isRead());
+        }
+
+        @Test
+        @DisplayName("존재하지 않는  채팅방이면 예외를 발생시킨다.")
+        void roomMessages_not_found_chatRoom() {
+            // given
+            given(chatRoomRepository.findById(roomId))
+                    .willReturn(Optional.empty());
+
+            // when
+            // then
+            assertThatThrownBy(() ->
+                    chatRoomService.roomMessages(roomId, customPageRequest))
+                    .isInstanceOf(ChatException.class)
+                    .hasMessageContaining(NOT_FOUND_CHATROOM.getErrorMessage());
+
+        }
+    }
+
+    @Nested
+    @DisplayName("내 채팅방 목록 조회 메서드는")
+    class MyChatRoomMethod {
+        String authId = "1";
+        CustomPageRequest customPageRequest = new CustomPageRequest(1);
+        Member member = Member.builder().build();
+        Pageable pageable = PageRequest.of(customPageRequest.page(), 10);
+
+        List<MyChatRoomResponse> contents = List.of(MyChatRoomResponse.builder()
+                .meetingName("meeting name")
+                .category(RestaurantCategory.ASIAN_CUISINE)
+                .regin("지역")
+                .roomId(1L)
+                .content("recent message")
+                .isRead(false)
+                .build());
+
+        @Test
+        @DisplayName("존재하는 회원이면 열려있는 채팅방을 조회할 수 있다.")
+        void roomMessages() {
+            // given
+            given(customUserDetailService.findByIdOrThrow(Long.parseLong(authId)))
+                    .willReturn(member);
+
+            given(meetingParticipantRepository.findAllMyChatRooms(member, pageable))
+                    .willReturn(new SliceImpl<>(contents, pageable, false));
+
+            // when
+            Slice<MyChatRoomResponse> myChatRoomResponses =
+                    chatRoomService.myChatRooms(authId, customPageRequest);
+
+            // then
+            assertEquals(1, myChatRoomResponses.getContent().size());
+        }
+
+        @Test
+        @DisplayName("존재하지 않는 회원이면 예외를 발생시킨다.")
+        void roomMessages_not_found_chatRoom() {
+            // given
+            given(customUserDetailService.findByIdOrThrow(Long.parseLong(authId)))
+                    .willThrow(new MemberException(NOT_FOUND_MEMBER));
+
+            // when
+            // then
+            assertThatThrownBy(() ->
+                    chatRoomService.myChatRooms(authId, customPageRequest))
+                    .isInstanceOf(MemberException.class)
+                    .hasMessageContaining(NOT_FOUND_MEMBER.getErrorMessage());
+        }
+    }
+}
