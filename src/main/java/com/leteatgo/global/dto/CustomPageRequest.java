0f08f5b548ed2f2cf48c537079dfd79756da34e1
do5do <do151015@gmail.com>
2024-02-06 20:54:58 +0900

    refactor: CustomPageRequest 예외 조건 추가

--- src/main/java/com/leteatgo/global/dto/CustomPageRequest.java
+++ src/main/java/com/leteatgo/global/dto/CustomPageRequest.java
@@ -9,7 +9,7 @@ public record CustomPageRequest(
     public static final int PAGE_SIZE = 10;
 
     public CustomPageRequest {
-        if (ObjectUtils.isEmpty(page)) {
+        if (ObjectUtils.isEmpty(page) || page == 0) {
             page = 0;
         } else {
             page -= 1; // request page start 1
