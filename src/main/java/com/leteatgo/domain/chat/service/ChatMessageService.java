e3e876a73e482a34cb216afb832ad69eb2ba97c2
dohee Kim <do151015@gmail.com>
2024-02-06 10:47:48 +0900

    [#30] 채팅방 메세지 전송 수정, 에러 메세지 세분화 (#32)
    * refactor: 구독한 채팅방으로만 메세지 전달 되도록 수정
    
    * refactor: 내 채팅방 조회 projection 생성자로 변경

--- src/main/java/com/leteatgo/domain/chat/service/ChatMessageService.java
+++ src/main/java/com/leteatgo/domain/chat/service/ChatMessageService.java
@@ -3,6 +3,7 @@ package com.leteatgo.domain.chat.service;
 import static com.leteatgo.global.exception.ErrorCode.NOT_FOUND_CHATROOM;
 
 import com.leteatgo.domain.chat.dto.ChatMessageDto;
+import com.leteatgo.domain.chat.dto.response.ChatMessageResponse;
 import com.leteatgo.domain.chat.entity.ChatMessage;
 import com.leteatgo.domain.chat.entity.ChatRoom;
 import com.leteatgo.domain.chat.exception.ChatException;
@@ -11,12 +12,9 @@ import com.leteatgo.domain.chat.repository.ChatRoomRepository;
 import com.leteatgo.domain.member.entity.Member;
 import com.leteatgo.global.security.CustomUserDetailService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-@Slf4j
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 @Service
@@ -26,17 +24,17 @@ public class ChatMessageService {
     private final ChatRoomRepository chatRoomRepository;
     private final CustomUserDetailService userDetailService;
 
-    @Async
     @Transactional
-    public void saveMessage(ChatMessageDto message, String authId) {
+    public ChatMessageResponse saveMessage(ChatMessageDto message, String authId, Long roomId) {
         Member sender = userDetailService.findByIdOrThrow(Long.parseLong(authId));
-        ChatRoom chatRoom = chatRoomRepository.findById(message.roomId())
+
+        ChatRoom chatRoom = chatRoomRepository.findById(roomId)
                 .orElseThrow(() -> new ChatException(NOT_FOUND_CHATROOM));
 
         ChatMessage chatMessage = message.toEntity();
         chatMessage.setSender(sender);
         chatMessage.setChatRoom(chatRoom);
 
-        chatMessageRepository.save(chatMessage);
+        return ChatMessageResponse.fromEntity(chatMessageRepository.save(chatMessage));
     }
 }
