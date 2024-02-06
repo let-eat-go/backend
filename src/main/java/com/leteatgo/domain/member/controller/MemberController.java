d31b6a576f8e8d36f7f5a585a67c8378c7883411
do5do <do151015@gmail.com>
2024-02-07 00:14:31 +0900

    style: 주석 추가

--- src/main/java/com/leteatgo/domain/member/controller/MemberController.java
+++ src/main/java/com/leteatgo/domain/member/controller/MemberController.java
@@ -34,6 +34,12 @@ public class MemberController {
 
     private final MemberService memberService;
 
+    /**
+     * 내 정보 조회
+     *
+     * @param userDetails 인증 유저
+     * @return 프로필 정보
+     */
     @RoleUser
     @GetMapping("/me")
     public ResponseEntity<MemberProfileResponse> myInformation(
@@ -42,6 +48,14 @@ public class MemberController {
                 Long.parseLong(userDetails.getUsername())));
     }
 
+    /**
+     * 회원 수정
+     *
+     * @param request     수정 데이터
+     * @param profile     프로필 이미지 파일
+     * @param userDetails 인증 유저
+     * @return void
+     */
     @RoleUser
     @PatchMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<Void> updateInfo(
@@ -52,6 +66,12 @@ public class MemberController {
         return ResponseEntity.ok().build();
     }
 
+    /**
+     * 회원 삭제
+     *
+     * @param userDetails 인증 유저
+     * @return void
+     */
     @RoleUser
     @DeleteMapping
     public ResponseEntity<Void> deleteMember(
@@ -60,6 +80,14 @@ public class MemberController {
         return ResponseEntity.ok().build();
     }
 
+    /**
+     * 내 모임 목록 조회
+     *
+     * @param type        조회 타입(모임 예정, 모임 완료, 내가 생성한 모임)
+     * @param request     요청 페이지
+     * @param userDetails 인증 유저
+     * @return 모임 목록
+     */
     @RoleUser
     @GetMapping("/meetings/me")
     public ResponseEntity<SliceResponse<MyMeetingsResponse>> myMeetings(
@@ -71,6 +99,12 @@ public class MemberController {
                         Long.parseLong(userDetails.getUsername()))));
     }
 
+    /**
+     * 타 회원 조회
+     *
+     * @param memberId 타 회원 id
+     * @return 프로필 정보
+     */
     @RoleUser
     @GetMapping("/{memberId}")
     public ResponseEntity<MemberProfileResponse> memberProfile(
