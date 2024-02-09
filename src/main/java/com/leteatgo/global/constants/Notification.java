eadec046df183a22fbc287e1b5aef4bd425662e5
Kingseungil <flykim511@naver.com>
2024-02-09 14:19:09 +0900

    chore: (constants) NotificationMessage -> Notification로 변경

--- src/main/java/com/leteatgo/global/constants/Notification.java
+++ src/main/java/com/leteatgo/global/constants/Notification.java
@@ -1,8 +1,9 @@
 package com.leteatgo.global.constants;
 
-public class NotificationMessage {
+public class Notification {
 
     public final static String SUBSCRIBE = "SSE Subscribe Success";
     public final static String MEETING_CANCEL = "[%s] 모임이 취소되었습니다.";
+    public static final String NOTIFICATION_QUEUE = "notification.queue";
 
 }
