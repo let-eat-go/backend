4abe297f4f6e2e1a17861686cb522cda25026669
do5do <do151015@gmail.com>
2024-02-14 18:44:32 +0900

    test: fallback 메서드 테스트

--- src/test/java/com/leteatgo/domain/tastyrestaurant/service/TastyRestaurantServiceTest.java
+++ src/test/java/com/leteatgo/domain/tastyrestaurant/service/TastyRestaurantServiceTest.java
@@ -5,6 +5,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 import com.leteatgo.domain.tastyrestaurant.dto.request.SearchRestaurantsRequest;
 import com.leteatgo.domain.tastyrestaurant.dto.response.PopularKeywordsResponse;
@@ -13,10 +15,14 @@ import com.leteatgo.domain.tastyrestaurant.dto.response.SearchRestaurantsRespons
 import com.leteatgo.domain.tastyrestaurant.dto.response.VisitedRestaurantResponse;
 import com.leteatgo.domain.tastyrestaurant.entity.TastyRestaurant;
 import com.leteatgo.domain.tastyrestaurant.repository.TastyRestaurantRepository;
+import com.leteatgo.global.dto.CustomPageRequest;
 import com.leteatgo.global.external.searchplace.client.RestaurantSearcher;
 import com.leteatgo.global.external.searchplace.client.kakao.dto.KakaoRestaurantsResponse;
 import com.leteatgo.global.external.searchplace.client.kakao.dto.KakaoRestaurantsResponse.Document;
 import com.leteatgo.global.external.searchplace.client.kakao.dto.KakaoRestaurantsResponse.Meta;
+import io.github.resilience4j.circuitbreaker.CallNotPermittedException;
+import io.github.resilience4j.circuitbreaker.CircuitBreaker;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -25,6 +31,9 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.SliceImpl;
 
 @ExtendWith(MockitoExtension.class)
 class TastyRestaurantServiceTest {
@@ -61,10 +70,20 @@ class TastyRestaurantServiceTest {
 
         Meta meta = new Meta(true, 1234);
 
+        SearchRestaurantsRequest request = SearchRestaurantsRequest.builder()
+                .keyword(keyword)
+                .latitude(127.06283)
+                .longitude(37.51432)
+                .build();
+
         @Test
         @DisplayName("성공 - 키워드 검색")
         void searchRestaurants() {
             // given
+            SearchRestaurantsRequest request = SearchRestaurantsRequest.builder()
+                    .keyword(keyword)
+                    .build();
+
             given(searchRestaurantClient.searchRestaurants(keyword, 1,
                     null, null, null, null))
                     .willReturn(new KakaoRestaurantsResponse(documents, meta));
@@ -72,10 +91,6 @@ class TastyRestaurantServiceTest {
             doNothing().when(redisRankingService).saveSearchKeyword(keyword);
 
             // when
-            SearchRestaurantsRequest request = SearchRestaurantsRequest.builder()
-                    .keyword(keyword)
-                    .build();
-
             SearchRestaurantsResponse response = tastyRestaurantService.searchRestaurants(
                     request);
 
@@ -95,18 +110,47 @@ class TastyRestaurantServiceTest {
             doNothing().when(redisRankingService).saveSearchKeyword(any());
 
             // when
-            SearchRestaurantsRequest request = SearchRestaurantsRequest.builder()
-                    .keyword(keyword)
-                    .latitude(127.06283)
-                    .longitude(37.51432)
-                    .build();
-
             SearchRestaurantsResponse response = tastyRestaurantService.searchRestaurants(
                     request);
 
             // then
             assertEquals(1, response.pagination().currentPage());
             assertEquals(KOREAN_CUISINE, response.contents().get(0).category());
+        }
+
+        @Test
+        @DisplayName("fallback 메서드")
+        void searchRestaurants_fallback() {
+            // given
+            Pageable pageable = PageRequest.of(request.page() - 1,
+                    CustomPageRequest.PAGE_SIZE);
+
+            List<TastyRestaurant> contents = List.of(TastyRestaurant.builder()
+                    .name("삼환소한마리")
+                    .category(KOREAN_CUISINE)
+                    .phoneNumber("02-545-2429")
+                    .roadAddress("도로명")
+                    .landAddress("지번")
+                    .latitude(127.06283102249932)
+                    .longitude(37.514322572335935)
+                    .restaurantUrl("http://place.map.kakao.com/8137464")
+                    .build());
+
+            given(tastyRestaurantRepository.searchRestaurants(request, pageable))
+                    .willReturn(new SliceImpl<>(contents, pageable, false));
+
+            // when
+            CallNotPermittedException callNotPermittedException =
+                    CallNotPermittedException.createCallNotPermittedException(
+                    CircuitBreaker.of("searchRestaurants", CircuitBreakerConfig.ofDefaults()));
+
+            SearchRestaurantsResponse response = tastyRestaurantService.searchRestaurantsFallback(
+                    request, callNotPermittedException);
+
+            // then
+            verify(tastyRestaurantRepository, times(1))
+                    .searchRestaurants(request, pageable);
+            assertEquals(1, response.pagination().currentPage());
         }
     }
 
