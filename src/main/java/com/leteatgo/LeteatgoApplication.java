1f06a0208e2304d3a9e0be94bc2be11bbe2a0212
Kingseungil <flykim511@naver.com>
2024-02-26 21:24:24 +0900

    fix: springboot timezone 변경

--- src/main/java/com/leteatgo/LeteatgoApplication.java
+++ src/main/java/com/leteatgo/LeteatgoApplication.java
@@ -1,10 +1,18 @@
 package com.leteatgo;
 
+import static java.util.TimeZone.*;
+
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LeteatgoApplication {
+
+    @PostConstruct
+    void started() {
+        setDefault(getTimeZone("Asia/Seoul"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(LeteatgoApplication.class, args);
